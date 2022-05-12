45df95b23953c1228a88a30354c7caa29bcff85a
Aria <ariacasellas@inspladelestany.cat>
2022-05-12 18:10:53 +0100

    Actualitzant el software

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import threading
 import json
 import os
+import time
+from sensors.humitat_temperatura import HumitatTemperatura
+from sensors.ultrasons import Ultrasons
+from sensors.moisure import Moisure
+import RPi.GPIO as GPIO
+
+## No es toca s'ha de posar en aquest mode abans d'utilitzar qualsevol sensor
+GPIO.setmode(GPIO.BOARD)
 
 config_path = os.environ["CONFIG_PATH"]
 config_file = open(config_path)
 config = json.load(config_file)
-print(config)
+print(config["sensors"][0]["events"][0])
+
+sensor = Moisure()
+x = threading.Thread(target=Moisure)
+while True:
+    print(sensor.read())
+    time.sleep(1)
--- sensors/moisure.py
+++ sensors/moisure.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 
 class Moisure:
-    def __init__(self, pin=37):
+    def __init__(self, pin=26):
         self.pin = pin
+        GPIO.setup(self.pin, GPIO.IN)
+
     
     def read(self):
       return GPIO.input(self.pin) == 0
