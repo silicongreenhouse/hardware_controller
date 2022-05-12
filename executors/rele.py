d0e17e6d686bec4751fbb99369442465c9540d7b
4strodev <jumarurex@gmail.com>
2022-05-12 14:54:46 +0200

    removing redundant code

--- executors/rele.py
+++ executors/rele.py
@@ -2,30 +2,11 @@ import RPi.GPIO as GPIO
 
 class Relay:
    def __init__(self, pin):
+      GPIO.setup(self.pin, GPIO.OUT)
       self.pin = pin
    
    def close(self):
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(self.pin, GPIO.OUT)
-      
-      try:
-         GPIO.output(self.pin, GPIO.HIGH)
-      except KeyboardInterrupt:
-         print("Keyboard interrupt")
-      except:
-         print("Some error")
-      finally:
-         GPIO.cleanup()
+      GPIO.output(self.pin, GPIO.HIGH)
 
    def open(self):
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(self.pin, GPIO.OUT)
-      
-      try:
-         GPIO.output(self.pin, GPIO.LOW)
-      except KeyboardInterrupt:
-         print("Keyboard interrupt")
-      except:
-         print("Some error")
-      finally:
-         GPIO.cleanup()
+      GPIO.output(self.pin, GPIO.LOW)
