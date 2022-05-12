8fa4580b35b267e2fa19ccb553932aa42e73194a
4strodev <jumarurex@gmail.com>
2022-05-12 14:55:17 +0200

    preparing main file

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-from sys import exec_prefix
-import time
-from sensors.humitat_temperatura import HumitatTemperatura
-from executors.rele import Relay
+import threading
+import json
+import os
 
-from sensors.ultrasons import Ultrasons
-
-
-dht_sensor = HumitatTemperatura(4)
-rele = Relay(17)
-ultrasons = Ultrasons()
-
-while True:
-  humidity, temperature = dht_sensor.read()
-  print(f'{humidity}% {temperature}*C')
-  nivell_aigua = ultrasons.mesura()
-  try:
-    rele.open()
-  except KeyboardInterrupt:
-    print("Keyboard interrupt")
-  except:
-    print("some error")
-
-  # finally:
-  #   rele.open()
-  #   rele.cleanup()
-
-  print(nivell_aigua)
-
-  time.sleep(1)
+config_path = os.environ["COONFIG_PATH"]
+config_file = open(config_path)
+config = json(config_file)
+print(config)
